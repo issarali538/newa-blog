ad60f9703918e0f7a5ae6522e01164382c825660
Issar Ali <issaralisia@gmail.com>
2021-07-17 22:23:08 -0700

    table is edited

--- back/post.php
+++ back/post.php
@@ -11,62 +11,18 @@ require "./header.php";
             <table class="table">
                <thead class="thead-dark">
                   <tr>
-                     <th scope="col">#</th>
-                     <th scope="col">First</th>
-                     <th scope="col">Last</th>
-                     <th scope="col">Handle</th>
+                     <th scope="col">id</th>
+                     <th scope="col">Post Title</th>
+                     <th scope="col">Date</th>
                   </tr>
                </thead>
                <tbody>
                   <tr>
                      <th scope="row">1</th>
-                     <td>Mark</td>
-                     <td>Otto</td>
-                     <td>@mdo</td>
+                     <td>The quick brown fox jumps over the lazy dogs</td>
+                     <td>12/3/2021</td>
                   </tr>
-                  <tr>
-                     <th scope="row">2</th>
-                     <td>Jacob</td>
-                     <td>Thornton</td>
-                     <td>@fat</td>
-                  </tr>
-                  <tr>
-                     <th scope="row">3</th>
-                     <td>Larry</td>
-                     <td>the Bird</td>
-                     <td>@twitter</td>
-                  </tr>
-               </tbody>
-            </table>
-
-            <table class="table">
-               <thead class="thead-light">
-                  <tr>
-                     <th scope="col">#</th>
-                     <th scope="col">First</th>
-                     <th scope="col">Last</th>
-                     <th scope="col">Handle</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  <tr>
-                     <th scope="row">1</th>
-                     <td>Mark</td>
-                     <td>Otto</td>
-                     <td>@mdo</td>
-                  </tr>
-                  <tr>
-                     <th scope="row">2</th>
-                     <td>Jacob</td>
-                     <td>Thornton</td>
-                     <td>@fat</td>
-                  </tr>
-                  <tr>
-                     <th scope="row">3</th>
-                     <td>Larry</td>
-                     <td>the Bird</td>
-                     <td>@twitter</td>
-                  </tr>
+                 
                </tbody>
             </table>
          </div>
